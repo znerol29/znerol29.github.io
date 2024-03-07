ff29a1644fe25b1362c2444db76ced8b55502156
Lorenz <160867235+znerol29@users.noreply.github.com>
2024-03-07 22:22:30 +0000

    Lightbox working

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-gsap.registerPlugin(ScrollTrigger) 
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
 
-gsap.fromTo(".page", { opacity: 1 }, { opacity: 0, duration: 5 });
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
